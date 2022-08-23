d62df294ea1edc1231fe4161af4cf46b32e65158
LOKESH RAMDEV BOHARA <lokeshramdevbohara999@gmail.com>
2022-08-23 14:29:42 +0530

    Made the file structure of the Project

---
+++ accountManagement/src/main/java/com/example/accountManagement/service/TransactionService.java
@@ -0,0 +1,5 @@
+package com.example.accountManagement.service;
+
+public interface TransactionService {
+
+}
---
+++ accountManagement/src/main/java/com/example/accountManagement/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,5 @@
+package com.example.accountManagement.service.impl;
+
+public class TransactionServiceImpl {
+
+}
